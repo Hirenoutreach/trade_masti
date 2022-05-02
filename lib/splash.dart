6f61c8e31f1955fea5a9a3da153a29438c4c5f04
yagnesh maniya <hiren.outreach@gmail.com>
2022-05-02 16:27:05 +0530

    Splash Screen Added

--- lib/splash.dart
+++ lib/splash.dart
@@ -1,11 +1,38 @@
 import 'package:flutter/material.dart';
+import './home.dart';
 
-class Splash extends StatelessWidget {
+class Splash extends StatefulWidget {
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState() {
+    super.initState();
+    _navigatetoHome();
+  }
+
+  _navigatetoHome() async {
+    await Future.delayed(Duration(milliseconds: 5500), () {});
+    Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+            builder: (context) => MyHomePage(
+                  title: "TradeMasti",
+                )));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        child: Text("TradeMasti"),
+    return Scaffold(
+      body: Center(
+        child: Container(
+          child: Text(
+            "TradeMasti",
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+        ),
       ),
     );
   }
