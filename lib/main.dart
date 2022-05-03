69dca87893570d8f351b73031bfa28a1c50d7fd1
yagnesh maniya <hiren.outreach@gmail.com>
2022-05-03 11:37:50 +0530

    routes set

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
-import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:page_transition/page_transition.dart';
 
-import './models/home.dart';
 import './models/anisplash.dart';
 
 void main() {
@@ -19,15 +16,10 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.cyan,
       ),
-      home: AnimatedSplashScreen(
-        splash: AniSplash(),
-        duration: 2500,
-        nextScreen: MyHomePage(title: "Trade Masti"),
-        backgroundColor: Color.fromARGB(255, 211, 240, 254),
-        animationDuration: Duration(milliseconds: 1500),
-        splashTransition: SplashTransition.fadeTransition,
-        pageTransitionType: PageTransitionType.leftToRight,
-      ),
+      initialRoute: 'home',
+      routes: {
+        'home': (context) => AniSplash(),
+      },
     );
   }
 }
--- lib/models/anisplash.dart
+++ lib/models/anisplash.dart
@@ -1,17 +1,24 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
+
+import 'home.dart';
 
 class AniSplash extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: AspectRatio(
-        aspectRatio: 25 / 25,
-        child: Image(
-          image: AssetImage(
-            'assets/image/WhatsApp Image 2022-04-27 at 3.02.03 PM.jpg',
-          ),
-        ),
+    return AnimatedSplashScreen(
+      splash: Image.asset(
+        'assets/image/WhatsApp Image 2022-04-27 at 3.02.03 PM.jpg',
+        scale: 0.1,
+        fit: BoxFit.fill,
       ),
+      duration: 2500,
+      nextScreen: MyHomePage(title: "Trade Masti"),
+      backgroundColor: Color.fromARGB(255, 211, 240, 254),
+      animationDuration: Duration(milliseconds: 1500),
+      // splashTransition: SplashTransition.fadeTransition,
+      pageTransitionType: PageTransitionType.leftToRight,
     );
   }
 }
